2520f43cc523fd86740448bf6ea67399961fc06a
Kelvin A. Moreno <kelvin.morenog28@gmail.com>
2024-02-06 18:45:04 -0300

    terminar plantillas [update] y [delete] .html

--- project/apps/core/views.py
+++ project/apps/core/views.py
@@ -71,43 +71,51 @@ class ConsultaListView(ListView):
 class ClienteUpdateView(UpdateView):
     model = Cliente
     form_class = ClienteForm
+    template_name = 'core/clientes_form.html'
     success_url = 'list'
 
 
 class DoctorUpdateView(UpdateView):
     model = Doctor
     form_class = DoctorForm
+    template_name = 'core/doctores_form.html'
     success_url = 'list'  
 
 
 class MascotaUpdateView(UpdateView):
     model = Mascota
     form_class = MascotaForm
+    template_name = 'core/mascotas_form.html'
     success_url = 'list'
 
 
 class ConsultaUpdateView(UpdateView):
     model = Consulta
     form_class = ConsultaForm
+    template_name = 'core/consultas_form.html'
     success_url = 'list'
 
 
 ## * Delete Models (Delete)
 class ClienteDeleteView(DeleteView):
     model = Cliente
+    template_name = 'core/clientes_delete.html'
     success_url = 'list'
 
 
 class DoctorDeleteView(DeleteView):
     model = Doctor
+    template_name = 'core/doctores_delete.html'
     success_url = 'list'
 
 
 class MascotaDeleteView(DeleteView):
     model = Mascota
+    template_name = 'core/mascotas_delete.html'
     success_url = 'list'
 
 
 class ConsultaDeleteView(DeleteView):
     model = Consulta
+    template_name = 'core/consultas_delete.html'
     success_url = 'list'
